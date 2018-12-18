26d9de2c3962a2f3567207a732ec1b0206229bd8
gjgd <gjgd@protonmail.com>
2018-12-19 00:26:06 +0100

    Switch to web3 1.0

--- index.spec.js
+++ index.spec.js
@@ -1,45 +1,38 @@
-const resolve = require('did-resolver');
-const ethrDidResolver = require('ethr-did-resolver');
-
-const register = ethrDidResolver.default;
-const EthrDID = require('ethr-did');
-const Contract = require('truffle-contract');
-const DidRegistryContract = require('ethr-did-registry');
+jest.useFakeTimers();
 const Web3 = require('web3');
+const contract = require('truffle-contract');
+const resolver = require('did-resolver');
+const EthrDID = require('ethr-did');
+const didRegistryContract = require('ethr-did-registry');
+const registerEthrDidToResolver = require('ethr-did-resolver').default;
+const HttpProvider = require('ethjs-provider-http');
 
 describe('With Ganache', () => {
   let provider;
   let web3;
   let registryOwner;
+  let accounts;
 
   beforeAll(async () => {
-    provider = new Web3.providers.HttpProvider('http://localhost:8545');
+    provider = new HttpProvider('http://localhost:8545');
     web3 = new Web3();
     web3.setProvider(provider);
-    const getAccounts = () => new Promise((_resolve, reject) => {
-      web3.eth.getAccounts((error, accounts) => {
-        if (error) {
-          reject(error);
-        } else {
-          _resolve(accounts);
-        }
-      });
-    });
-    const accounts = await getAccounts();
+    accounts = await web3.eth.getAccounts();
     registryOwner = accounts[0];
   });
 
   let registryAddress;
+  let registry;
   describe('Set up registry', () => {
     beforeAll(async () => {
-      const DidReg = Contract(DidRegistryContract);
+      const DidReg = contract(didRegistryContract);
       DidReg.setProvider(provider);
-      const registry = await DidReg.new({
+      registry = await DidReg.new({
         from: registryOwner,
         gasPrice: 100000000000,
         gas: 4712388,
       });
-      registryAddress = registry.address.toLowerCase();
+      registryAddress = registry.address;
     });
 
     it('should deploy the registry contract', () => {
@@ -48,11 +41,15 @@ describe('With Ganache', () => {
   });
 
   let did;
+  let ethrDid;
+  let myAddress;
   describe('Create an Ethr DID', () => {
     beforeAll(async () => {
       const keyPair = EthrDID.createKeyPair();
-      const ethrDid = new EthrDID({
+      myAddress = keyPair.address;
+      ethrDid = new EthrDID({
         address: keyPair.address,
+        privateKey: keyPair.privateKey,
         provider,
         registry: registryAddress.toLowerCase(),
       });
@@ -63,19 +60,27 @@ describe('With Ganache', () => {
       expect(did).toBeDefined();
     });
 
-    it('should fail to resolve without setting up the resolver first', () => expect(resolve(did)).rejects.toThrow("Unsupported DID method: 'ethr'"));
+    it('should have the right owner', async () => {
+      const owner = await ethrDid.lookupOwner();
+      return expect(owner).toBe(myAddress);
+    });
+
+    // eslint-disable-next-line
+    it('should fail to resolve without setting up the resolver first', () => {
+      return expect(resolver(did)).rejects.toThrow("Unsupported DID method: 'ethr'");
+    });
   });
 
   describe('Set up resolver', () => {
     beforeAll(() => {
-      register({
+      registerEthrDidToResolver({
         provider,
         registry: registryAddress,
       });
     });
 
     it('should resolve the did', async () => {
-      const didDocument = await resolve(did);
+      const didDocument = await resolver(did);
       expect(didDocument['@context']).toBeDefined();
       expect(didDocument.id).toBe(did);
     });
